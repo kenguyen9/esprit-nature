3cdfc110c0265b606f6998235a4d0cb613ab7fe6
Ken Nguyen <ken.nguyen@vesta-system.com>
2020-11-20 17:25:38 +0100

    initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { GameManagerComponent } from './game-manager/game-manager.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { IntroComponent } from './intro/intro.component';
+
+const routes: Routes = [
+  { path: '', component: MainComponent, pathMatch: 'full' },
+  { path: 'prologue', component: IntroComponent },
+  {
+    path: 'game', component: GameManagerComponent
+
+
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainComponent } from './main/main.component';
+import { AppRoutingModule } from './app-routing.module';
+import { IntroComponent } from './intro/intro.component';
+import { GameManagerComponent } from './game-manager/game-manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    IntroComponent,
+    GameManagerComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
